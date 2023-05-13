b1524ebab7f49d84c8949c1b7fb73f930c04df5b
AlainaMarta <alainamarta@gmail.com>
2023-05-13 20:46:28 -0300

    resolução do exercicio do dia2

---
+++ fundamentos/secao-04-DOM/dia2/script.js
@@ -0,0 +1,17 @@
+
+
+const title = document.createElement('h1');
+title.innerText = 'TrybeTrip - Agência de Viagens'
+document.body.appendChild(title);
+
+const main = document.createElement('main');
+main.className = 'main-content';
+document.body.appendChild(mainContent);
+
+const section = document.createElement('section');
+section.className = 'center-content';
+main.appendChild(centerContent);
+
+const p = document.createElement('p');
+p.innerText = 'Complicado';
+centerContent.appendChild(p);
